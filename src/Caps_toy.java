70fa8440a20bd1aa2773aff573491b9ed46eaf11
Maartje <maartje.swart@gmail.com>
2023-01-14 17:59:48 +0100

    Vereenvoudigd

--- src/Caps_toy.java
+++ src/Caps_toy.java
@@ -4,7 +4,7 @@ public class Caps_toy {
     public static void main(String[] args) {
         System.out.println("Welke tekst wil je omzetten?");
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        String input = scanner.nextLine().toLowerCase();
         char[] characterArray = input.toCharArray();
 
 
@@ -12,10 +12,7 @@ public class Caps_toy {
             if (x % 2 == 0 && characterArray[x] > 96 && characterArray[x] < 123) {
                 //karakters op even plaatsen zijn hoofdletters
                 characterArray[x] = (char) ((characterArray[x]) - 32);
-            } else if (x % 2 != 0 && characterArray[x] > 64 && characterArray[x] < 91) {
-                //karakters op oneven plaatsen zijn klein
-                characterArray[x] = (char) ((characterArray[x]) + 32);
-            } else characterArray[x] = characterArray[x];
+            }
 
             System.out.print(characterArray[x]);
         }
